aaa55fd9fc7b329118dfe4e6df6dd94283bbaae0
Easy solutions <Easy solutions@ezs013>
2017-09-18 19:15:28 +0530

    Send mail(Report to mail), Created folder with result for each execution

--- eResidue_CV_eDocs_Automation/src/com/eResidue/Equipment/Equipment_Name.java
+++ eResidue_CV_eDocs_Automation/src/com/eResidue/Equipment/Equipment_Name.java
@@ -1,34 +1,46 @@
 package com.eResidue.Equipment;
 
-import java.io.IOException;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
-import com.eResidue.Utils.Constant;
 import com.eResidue.Utils.Utils;
 
 public class Equipment_Name {
-	public WebDriver driver = Constant.driver ; 
+	//public WebDriver driver = Constant.driver ; 
+	final static String FILE_PATH = "C:\\Users\\Easy solutions\\git\\eDocs\\eResidue_CV_eDocs_Automation\\eResidue_eDocs_TestCase_result.xlsx";
 	
 	
-	@Test(priority=1)
+	/*@Test(priority=1)
 	public void getWebdriver() {
 		//Utils.getWebDriverInstance();
 		driver.get("https://www.google.co.in");
 	    System.out.println("First Commmit Test");	
 	  }
+	*/
 	
 	@Test(priority=2)
-	public void getWebdriver1() throws InterruptedException, IOException {
+	public void google() throws Exception {
 		
-		Thread.sleep(500);
+		/*Thread.sleep(500);
 		driver.findElement(By.xpath(".//*[@id='gbw']/div/div/div[1]/div[1]/a")).click();
-		Utils.getScreenShot(Constant.SCREENSHOT_FOLDER + "\\Test1.PNG ");
+		Utils.getScreenShot(Constant.SCREENSHOT_FOLDER + "\\Test1.PNG ");*/
 		
+		XSSFWorkbook workbook = Utils.getExcelSheet();
+		XSSFSheet sheet = workbook.getSheetAt(1);
+		 
+		String Eq_name_valid = sheet.getRow(5).getCell(5).getStringCellValue();
+		System.out.println(Eq_name_valid);
+		
+		Cell resultcell = sheet.getRow(5).getCell(7);
+		resultcell.setCellValue(Eq_name_valid);
+		
+		Utils.writeTooutputFile(workbook); // write and close workbook
+			
 	  }
 	
-		
+	
+			
 }
 
--- eResidue_CV_eDocs_Automation/src/com/eResidue/Utils/Utils.java
+++ eResidue_CV_eDocs_Automation/src/com/eResidue/Utils/Utils.java
@@ -1,17 +1,23 @@
 package com.eResidue.Utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class Utils {
 
+	
+	//Method of screenshot
 	public static void getScreenShot(String file) throws IOException {
 		try {
 
@@ -26,11 +32,36 @@ public class Utils {
 		}
 	}
 
-	/*
-	 * public static XSSFSheet getExcelSheet() throws IOException {
-	 * FileInputStream file = new FileInputStream(Constant.EXCEL_PATH);
-	 * XSSFWorkbook workbook = new XSSFWorkbook(file); XSSFSheet sheet =
-	 * workbook.getSheetAt(0); return sheet; }
-	 */
+	
+	  /*public static XSSFSheet getExcelSheet() throws IOException 
+	  {
+	  FileInputStream file = new FileInputStream(Constant.EXCEL_PATH);
+	  @SuppressWarnings("resource")
+	  XSSFWorkbook workbook = new XSSFWorkbook(file); 
+	  XSSFSheet sheet = workbook.getSheetAt(1); 
+	  return sheet; 
+	  }*/
+	
+	  
+	  //get xlsx sheet
+	  public static XSSFWorkbook getExcelSheet() throws IOException 
+	  {
+	  FileInputStream file = new FileInputStream(Constant.EXCEL_PATH);
+	  XSSFWorkbook workbook = new XSSFWorkbook(file); 
+	  return workbook; 
+	  }
+	    
+	  
+	  // Write output and close workbook
+	  public static void writeTooutputFile(Workbook workbook) throws IOException {
+			try {
+				FileOutputStream outFile = new FileOutputStream(new File("C:\\Users\\Easy solutions\\git\\eDocs\\eResidue_CV_eDocs_Automation\\eResidue_eDocs_TestCase_result.xlsx"));
+				workbook.write(outFile);
+				outFile.close();
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+	 
 
 }
